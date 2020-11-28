da1e1e8e4415a21033476995db1126a80104fa06
SpiralP <SpiralP@users.noreply.github.com>
2020-11-28 03:38:30 +0000

    deploy: 6b99d0a22f75b928afe012a3a42b96d2fe217ff6

--- implementors/failure/trait.Fail.js
+++ implementors/failure/trait.Fail.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["failure"] = [];
-implementors["open_hardware_monitor_sys"] = [{"text":"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"open_hardware_monitor_sys/enum.FFIWCharPtrError.html\" title=\"enum open_hardware_monitor_sys::FFIWCharPtrError\">FFIWCharPtrError</a>","synthetic":false,"types":["open_hardware_monitor_sys::bindings::FFIWCharPtrError"]},{"text":"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"open_hardware_monitor_sys/enum.ComputerError.html\" title=\"enum open_hardware_monitor_sys::ComputerError\">ComputerError</a>","synthetic":false,"types":["open_hardware_monitor_sys::ComputerError"]}];
+implementors["open_hardware_monitor_sys"] = [{"text":"impl Fail for FFIWCharPtrError","synthetic":false,"types":[]},{"text":"impl Fail for ComputerError","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
